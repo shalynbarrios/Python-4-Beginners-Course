63c7360c5fb7c2fcbc05ee10e01d8e02c308c21b
Shalyn Barrios <165353062+shalynbarrios@users.noreply.github.com>
2024-12-28 22:57:14 -0600

    firstlesson

--- Lesson0_Introduction.py
+++ Lesson0_Introduction.py
@@ -1,1 +1,109 @@
 # Hello, world!
+
+# This is Shay's Introduction to Python Course.
+# Let's jump right in.
+
+# -------------
+# Hello, world!
+# -------------
+
+# Why did I start with the phrase "Hello, world!" ?
+
+# In 1972, computer scientist Brian Kernighan's tutorial for programming started
+# by instructing students to write their following first line of code:
+
+print("Hello, world!")
+
+# Try it! Type it in your notebook and hit run.
+
+# I want you to ALWAYS type out what I type in your notebook.
+# The best way to learn is by doing. 
+# THIS MEANS DON'T COPY PASTE ! :)
+
+# So follow the tradition, and type print("Hello, world!").
+
+# Output: Hello, world!
+
+# What is the computer doing?
+# The computer is "reading" your code, line by line, top to bottom, left to right.
+# This is Python's execution order.
+
+# Code executes left to right, top to bottom, as standard English is read.
+# You will find that Python is a very readable language. Perfect for beginners!
+
+# -------------
+# The Print Function
+# -------------
+
+# What does print("Hello, world!") exactly mean?
+
+print()
+
+# ^ This is the print function.
+# A 'function' is a fancy word for "action."
+# The computer comes with predetermined basic functions or "actions."
+# So the print function is a specific action that the computer already knows what to do when you type it.
+
+# This print function prints whatever you would like into the terminal (the terminal is where you saw the previous output)
+
+print(5)
+
+# This code will print the number 5 into the terminal.
+# How? By running the print function, and inside the function, there is the number 5
+
+# -------------
+# The Print Function: Words
+# -------------
+
+# In Python, words are known as "strings"
+# A good way to remember this terminology is to think:
+# A word is a "string" of characters.
+
+print("Hello!")
+
+# So with Python terminology,
+# This print function prints the string Hello! into the terminal.
+
+# But Shay, what if I wanted the function to print with quotes?
+
+print("'Hello!")
+
+# This print function prints the string 'Hello!' into the terminal.
+
+# Now, I want you to understand takes everything you say 100% literal
+# What do I mean by that?
+
+print("5")
+
+# Even though you and I interpret this as the number 5, 
+# Python interprets this as a string 
+# Because remember, a string is a string of characters.
+
+# -------------
+# Recap
+# -------------
+
+# We learned that:
+# -- Python reads left to right, top to bottom
+# -- We can display information into the terminal by using the built-in print function
+# -- Words as you and I know them are known to Python as "strings"
+
+# -------------
+# Challenge
+# -------------
+
+print("Hello!")
+print("These strings are going to be printed from 1 to 3.")
+print("1")
+print("3")
+print(2)
+
+# -------------
+# Challenge: Solved
+# -------------
+
+print("Hello!")
+print("These strings are going to be printed from 1 to 3.")
+print("1")
+print("2")
+print("3")
