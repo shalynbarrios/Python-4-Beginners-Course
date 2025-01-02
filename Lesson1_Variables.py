89374f6c80ad245a7124f497b3e0f8b3de81fa11
Shalyn Barrios <165353062+shalynbarrios@users.noreply.github.com>
2025-01-01 18:23:29 -0600

    update

---
+++ Lesson1_Variables.py
@@ -0,0 +1,16 @@
+# Let's examine the following code:
+
+print("Hello, world!")
+print("Hello, world!")
+print("Hello, world!")
+print("Hello, world!")
+
+# It's not wrong, however -- 
+# The problem is that we want to keep reusing the same string multiple times in our code.
+# But as we can see, our code is pretty repetitive.
+
+# This is where variables come in.
+
+# -------------
+# Introduction to Variables
+# -------------
